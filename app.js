a2cf3e293e572fc1fd959d8d2be8b99b6bffd600
glitton <glcodeworks@gmail.com>
2023-06-13 16:46:43 -0500

    javascript functionality done, css next

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Ted Lasso API from https://tedlassoquotes.com/
+// Ted Lasso API from https://tedlassoquotes.com
 
 const url = "https://tedlassoquotes.com/v1/quote";
 
@@ -12,10 +12,22 @@ btn.addEventListener("click", () => {
 });
 
 const fetchTedLassoQuote = async () => {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  quoteResult.textContent = data.quote;
-  quoteAuthor.textContent = data.author;
-  img.src = data.profile_img;
+  quoteResult.textContent = "Loading ...";
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("there was an error");
+    }
+    const data = await response.json();
+    // console.log(data);
+    quoteResult.textContent = data.quote;
+    quoteAuthor.textContent = data.author;
+    img.src = data.profile_img;
+  } catch (error) {
+    quoteResult.textContent = "There was an error loading the quote.";
+  }
 };
+
+fetchTedLassoQuote();
